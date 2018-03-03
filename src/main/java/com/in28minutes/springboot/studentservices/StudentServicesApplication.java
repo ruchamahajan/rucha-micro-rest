9fc628e5e021b722213deae331b1138e7436e98a
Rucha Mahajan <rucha.mahajan@gmail.com>
2018-03-03 16:35:34 -0500

    Attempt at micro-services

--- src/main/java/com/in28minutes/springboot/studentservices/StudentServicesApplication.java
+++ src/main/java/com/in28minutes/springboot/studentservices/StudentServicesApplication.java
@@ -1,22 +1,30 @@
 package com.in28minutes.springboot.studentservices;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 //import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.ApplicationContext;
 
 
-@RestController
-@EnableAutoConfiguration
+
+@SpringBootApplication
+@EnableEurekaServer
 public class StudentServicesApplication {
 
 	
-	@RequestMapping("/hello")
 	String home() {
 		return "Hello World!";
 	}
 
 	public static void main(String[] args) {
+
+		ApplicationContext ctx = new SpringApplicationBuilder().bannerMode(Banner.Mode.CONSOLE).run(args);
+
 		SpringApplication.run(StudentServicesApplication.class, args);
 	}
 }
